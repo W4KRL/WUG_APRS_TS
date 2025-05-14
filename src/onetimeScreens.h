1fb57420fbc252d40f9856b52bf292d231b5bc45
Karl Berger <W4KRL@arrl.net>
2025-05-14 15:05:23 -0400

    mild cleanup

--- src/onetimeScreens.h
+++ src/onetimeScreens.h
@@ -1,11 +1,10 @@
 //! @file onetimeScreens.h
-//! @brief One-time screens
+//! @brief One-time screens shown at bootup
 //! @details This file contains the functions for the one-time screens
 //!          that are displayed on the TFT display.
 //! @author Karl Berger
 //! @date 2025-05-13
 
-//! @file onetimeScreens.h
 #ifndef ONETIME_SCREENS_H
 #define ONETIME_SCREENS_H
 
@@ -21,5 +20,4 @@ void splashScreen();
 void dataScreen();
 
 #endif // ONETIME_SCREENS_H
-
 // End of file
