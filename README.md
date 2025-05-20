30cab472f48abe941c3ae5a6efdb8e29f85bf56e
Karl Berger <w4krl@arrl.net>
2025-05-19 22:29:37 -0400

    Remove text to README.md

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # WUG_APRS_TS
 Connects Weather Underground feed to APRS and Thingspeak
+
+
+What to Include in README.md
+- Project Overview – Brief description of what your project does.
+- Installation Steps – Any software dependencies or setup requirements.
+- Device Connections – A simplified version of wiring or hardware requirements.
+- Basic Usage Instructions – How to run the program.
+- Links to Further Documentation – If needed, reference a more detailed 
+
+
+// Displays local weather from VEVOR 7-in-1 Weather Station
+// through Weather Underground PWS account
+/* Configured in credentials.h
+ ? Data Needed:
+ *   Wi-Fi credentials (SSID & Password)
+ *   WeatherUnderground.com API key
+ *   Olson timezone
+ *   APRS callsign & SSID
+ *   APRS passcode
+ *   ThingSpeak channel ID
+ */
+
+
+/*
+******************************************************
+**************** DEVICE CONNECTIONS ******************
+******************************************************
+!              WEMOS D1 MINI PINOUTS
+ /RST - TFT RST          TX - GPIO 1 not used
+   A0 - ADC not used     RX - GPIO 3 not used
+   D0 - GPIO 16 CS       D1 - GPIO 5 I2C SCL
+   D5 - GPIO 14 SCK      D2 - GPIO 4 I2C SDA
+   D6 - GPIO 12 MISO     D3 - GPIO 0 TFT_DC
+   D7 - GPIO 13 MOSI     D4 - GPIO 2 LED_BUILTIN
+   D8 - GPIO 15 SS      GND - return for sensor
+  3V3 - power to sensor  5V - not used
+                  |USB|
+
+ ! TFT = WEMOS 1.4 shield connections
+ ! Mod to screen moves CS from D4 to D0 to avoid LED flash
+ ! Used on kits starting August 2022
+  #define TFT_RST -1  // use -1 according to Wemos
+  #define TFT_DC D3        // GPIO 0
+  #define MOSI D7          // GPIO 13
+  #define SCK D5           // GPIO 14
+  #define TFT_LED NC       // no connection
+*/
+
+
+// this version accomodates modified and unmodified TFT screens
+// it must be compiled as one or the other
+// unmarked version has CS = D4
+// use version v2 with TFT_MOD CS = D0
+// expects AHT10 sensor
+
+//! Weather code from D1S-Display-WBIT-Mod.ino
+//! APRS code from D1M-WX2-APRS-TS.ino
+//! ThingSpeak code from XXXX
--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,28 @@
-//! In folder WUG_APRS_TS
-//! 12/26/2024 imported to Platformio from D1S-Display-WUG-VEVOR-APRS_24_12_22_WORKING.ino
-//! Synced with https://github.com/W4KRL/WUG_APRS_TS
-
-// this version accomodates modified and unmodified TFT screens
-// it must be compiled as one or the other
-// unmarked version has CS = D4
-// use version v2 with TFT_MOD CS = D0
-// expects AHT10 sensor
-
-//! Weather code from D1S-Display-WBIT-Mod.ino
-//! APRS code from D1M-WX2-APRS-TS.ino
-//! ThingSpeak code from XXXX
-
-// TODO read day/night indicator and choose json data accordingly
-// TODO add APRS bulletin for sunrise/sunset
-// TODO test WUG API responses for absence. Do not update WX data if response missing
-
-#define WUG_DEBUG //! uncomment this line for serial debug output
-
-// Displays local weather from VEVOR 7-in-1 Weather Station
-// through Weather Underground PWS account
-/* Configured in credentials.h
- ? Data Needed:
- *   Wi-Fi credentials (SSID & Password)
- *   WeatherUnderground.com API key
- *   Olson timezone
- *   APRS callsign & SSID
- *   APRS passcode
- *   ThingSpeak channel ID
+/**
+ * @file main.cpp
+ * @brief Main application for the Magnetic Loop Antenna Controller
+ * @details This program initializes communication with sensors and processes input data
+ *
+ * @author Karl Berger
+ * @date 2025-05-19
+ *
+ * @note Ensure the device is connected as described in README.md.
+ * 
+ * @todo read day/night indicator and choose json data accordingly
+ * @todo add APRS bulletin for sunrise/sunset
+ * @todo test WUG API responses for absence. Do not update WX data if response missing
+ * 
+ * @link https://github.com/W4KRL/WUG_APRS_TS
  */
 
-/*_____________________________________________________________________________
-   Copyright(c) 2018 - 2025 Karl Berger dba IoT Kits https://w4krl.com
-
-   Permission is hereby granted, free of charge, to any person obtaining a copy
-   of this software and associated documentation files (the "Software"), to deal
-   in the Software without restriction, including without limitation the rights
-   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-   copies of the Software, and to permit persons to whom the Software is
-   furnished to do so, subject to the following conditions:
-
-   **The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.**
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-   SOFTWARE.
-   _____________________________________________________________________________
-*/
+#define WUG_DEBUG //! uncomment this line for serial debug output
 
 /*
 ******************************************************
 ******************** INCLUDES ************************
 ******************************************************
 */
-#include <Arduino.h>           // [builtin] PlatformIO
+#include <Arduino.h>           // Arduino functions
 #include "analogClock.h"       // analog clock functions
 #include "aphorismGenerator.h" // aphorism functions
 #include "aprsService.h"       // APRS functions
@@ -75,31 +39,6 @@
 #include "wifiConnection.h"    // Wi-Fi connection
 #include "wug_debug.h"         // debug print macro
 #include <TickTwo.h>           // v4.4.0 Stefan Staub https://github.com/sstaub/TickTwo
-
-/*
-******************************************************
-**************** DEVICE CONNECTIONS ******************
-******************************************************
-!              WEMOS D1 MINI PINOUTS
- /RST - TFT RST          TX - GPIO 1 not used
-   A0 - ADC not used     RX - GPIO 3 not used
-   D0 - GPIO 16 CS       D1 - GPIO 5 I2C SCL
-   D5 - GPIO 14 SCK      D2 - GPIO 4 I2C SDA
-   D6 - GPIO 12 MISO     D3 - GPIO 0 TFT_DC
-   D7 - GPIO 13 MOSI     D4 - GPIO 2 LED_BUILTIN
-   D8 - GPIO 15 SS      GND - return for sensor
-  3V3 - power to sensor  5V - not used
-                  |USB|
-
- ! TFT = WEMOS 1.4 shield connections
- ! Mod to screen moves CS from D4 to D0 to avoid LED flash
- ! Used on kits starting August 2022
-  #define TFT_RST -1  // use -1 according to Wemos
-  #define TFT_DC D3        // GPIO 0
-  #define MOSI D7          // GPIO 13
-  #define SCK D5           // GPIO 14
-  #define TFT_LED NC       // no connection
-*/
 
 //! Scheduled tasks
 TickTwo tmrWXcurrent(updateWXcurrent, WX_CURRENT_INTERVAL * 60 * 1000, 0, MILLIS);
@@ -138,8 +77,6 @@ void setup()
 ********************* LOOP ***************************
 ******************************************************
 */
-// Main loop function that handles periodic tasks, updates clocks, processes APRS bulletins,
-// and manages scheduled timers for weather updates and display frames.
 void loop()
 {
   events(); // ezTime events including autoconnect to NTP server
