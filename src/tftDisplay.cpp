1d89bd1ac1eb008ee7a53426b42bac04f330b5c6
Karl Berger <w4krl@arrl.net>
2025-05-19 20:41:37 -0400

    rationalize variables

--- src/tftDisplay.cpp
+++ src/tftDisplay.cpp
@@ -4,31 +4,41 @@
  */
 
 #include "tftDisplay.h"
+
+#include <Arduino.h>
 #include "Roboto_Bold_12.h"
 #include "Roboto_Bold_16.h"
 
 // Display configuration
+// Display dimensions (initialized in setup)
+const int USB_LEFT = 0;
+const int USB_DOWN = 1;
+const int USB_RIGHT = 2;
+const int USB_UP = 3;
+const int LEFT_COL = 6;
+const int RIGHT_COL = 125;
+const int HEADER_RAD = 8;
+const int HEADER_Y = 37;
+
 const int FRAME_ORIENTATION = USB_LEFT;
+
+//! Display instaiation
 TFT_eSPI tft;
 
 // Font resources
-const GFXfont* SmallBold = &Roboto_Bold_12;
-const GFXfont* LargeBold = &Roboto_Bold_16;
+const GFXfont *SmallBold = &Roboto_Bold_12;
+const GFXfont *LargeBold = &Roboto_Bold_16;
 
-// Display dimensions (initialized in setup)
 int SCREEN_W = 128;
 int SCREEN_H = 128;
 int SCREEN_W2 = SCREEN_W / 2;
 int SCREEN_H2 = SCREEN_H / 2;
-int LEFT_COL = 6;
-int RIGHT_COL = 125;
-int HEADER_RAD = 8;
-int HEADER_Y = 37;
 
-void setupTFTDisplay() {
+void setupTFTDisplay()
+{
   tft.init();
   tft.setRotation(FRAME_ORIENTATION);
-  
+
   // Update dimensions based on actual display
   SCREEN_W = tft.width();
   SCREEN_H = tft.height();
--- src/tftDisplay.h
+++ src/tftDisplay.h
@@ -10,30 +10,26 @@
 
 #include <TFT_eSPI.h> // [manager] v2.4.0 TFT_eSPI
 
-// Display orientation definitions
-#define USB_LEFT 0
-#define USB_DOWN 1
-#define USB_RIGHT 2
-#define USB_UP 3
+//! TFT instance
+extern TFT_eSPI tft;
 
-// Display constants
-extern const int FRAME_ORIENTATION;
+//! Display constants
+extern const int LEFT_COL;    // left margin
+extern const int RIGHT_COL;   // right margin
+extern const int HEADER_RAD;  // header corner radius
+extern const int HEADER_Y;    // header bottom row
+
+//! values read from display device
 extern int SCREEN_W;    // width in pixels
 extern int SCREEN_H;    // height in pixels
 extern int SCREEN_W2;   // half width
 extern int SCREEN_H2;   // half height
-extern int LEFT_COL;    // left margin
-extern int RIGHT_COL;   // right margin
-extern int HEADER_RAD;  // header corner radius
-extern int HEADER_Y;    // header bottom row
 
-// Font pointers
+//! Font pointers
 extern const GFXfont* SmallBold;
 extern const GFXfont* LargeBold;
 
-// TFT instance
-extern TFT_eSPI tft;
-
+//! Functions
 void setupTFTDisplay();
 
 #endif // TFT_DISPLAY_H
