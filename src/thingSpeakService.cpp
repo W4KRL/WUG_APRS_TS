f2f34ab35e88dc2de0e49a00b72fa7050bd4f848
Karl Berger <W4KRL@arrl.net>
2025-05-23 16:44:21 -0400

    Fix formatting of include statement for consistency

--- src/thingSpeakService.cpp
+++ src/thingSpeakService.cpp
@@ -11,7 +11,7 @@
 #include <WiFiClient.h>     // ThingSpeak connection
 #include "credentials.h"    // Wi-Fi and weather station credentials
 #include "weatherService.h" // weather data
-#include "wug_debug.h"          // debug print
+#include "wug_debug.h"      // debug print
 
 //! ************** THINGSPEAK ************************
 #define THINGSPEAK_SERVER "api.thingspeak.com" // ThingSpeak Server
