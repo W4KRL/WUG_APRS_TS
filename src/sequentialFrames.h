8dab997e8ed80589777b76ce59547cbebe103a08
Karl Berger <W4KRL@arrl.net>
2025-05-29 12:51:38 -0400

    Refactor sequentialFrames header and source files: update author and date, and improve include statements for clarity.

--- src/sequentialFrames.h
+++ src/sequentialFrames.h
@@ -1,17 +1,18 @@
 /**
  * @file sequentialFrames.h
+ * @author Karl Berger
+ * @date 2025-05-29
  * @brief Sequential frames for the TFT display
  * @details This file contains the functions for displaying sequential frames
  *          on the TFT display. The frames include weather data, almanac data,
  *          and clock data.
- * @author Karl Berger
- * @date 2025-05-16
  */
 
 #ifndef SEQUENTIAL_FRAMES_H
 #define SEQUENTIAL_FRAMES_H
 
 void updateFrame();
+void drawFramePanels(int top_background, int bottom_background);
 
 #endif // SEQUENTIAL_FRAMES_H
 // End of file
